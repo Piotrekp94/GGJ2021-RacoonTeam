e74ca2ae10918450b91f242a493418c4f9bab7ac
piotr <piotrekp94@gmail.com>
2021-01-29 12:11:42 +0100

    Update

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -5,17 +5,19 @@ public class MovementScript : MonoBehaviour
 {
     private static readonly int Speed = Animator.StringToHash("Speed");
     private static readonly int isStoppingHash = Animator.StringToHash("isStopping");
-    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int isTurningHash = Animator.StringToHash("isTurning");
+
     [SerializeField] private float speed;
 
     public Rigidbody rb;
     public Animator animator;
 
+    public bool isRunningLeft;
 
     public Vector3 _movement;
-    public bool isStopping = false;
-    public bool isTurning = false;
-    private bool isMoving = false;
+    public bool isStopping;
+    public bool isTurning;
+    private bool isMoving;
 
     private void Start()
     {
@@ -25,24 +27,57 @@ public class MovementScript : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            if (_movement.x > 0) isRunningLeft = true;
+            if (_movement.x < 0)
+                isTurning = true;
+            else
+                isTurning = false;
             isStopping = false;
             _movement = stepToDesiredSpeed(1f, 0.01f, _movement);
-            isMoving = true;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            if (_movement.x < 0) isRunningLeft = false;
+            if (_movement.x > 0)
+                isTurning = true;
+            else
+                isTurning = false;
+
+            
             isStopping = false;
             _movement = stepToDesiredSpeed(-1f, 0.01f, _movement);
-            isMoving = true;
         }
         else if (isMoving)
         {
+            isTurning = false;
             isStopping = true;
-            _movement = stepToDesiredSpeed(0, 0.02f, _movement);
+            _movement = stepToDesiredSpeed(0, 0.01f, _movement);
+        }
+
+        if (isRunningLeft)
+        {
+            if (transform.localScale.z > 0)
+            {
+                var localScale = transform.localScale;
+                localScale =
+                    new Vector3(localScale.x, localScale.y, -localScale.z);
+                transform.localScale = localScale;
+            }
+        } else
+        if (!isRunningLeft)
+        {
+            if (transform.localScale.z < 0)
+            {
+                var localScale = transform.localScale;
+                localScale =
+                    new Vector3(localScale.x, localScale.y, -localScale.z);
+                transform.localScale = localScale;
+            }
         }
 
         _movement.y = Input.GetAxisRaw("Vertical");
 
+        animator.SetBool(isTurningHash, isTurning);
         animator.SetBool(isStoppingHash, isStopping);
         animator.SetFloat(Speed, _movement.sqrMagnitude);
     }
@@ -61,6 +96,7 @@ public class MovementScript : MonoBehaviour
             return currentSpeed;
         }
 
+        isMoving = true;
         if (currentSpeed.x > desiredSpeed) step *= -1;
 
         if (step < 0)
